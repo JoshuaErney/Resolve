9f5e7d1ed429e50cd62ac35d218c8c60db69f95c
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2023-07-18 08:52:50 -0400

    reset styling to bare minimum

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".hamburger-menu");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click", () => {
-    if(window.innerWidth <= 768){
-        navLinks.classList.toggle("show");
-    }
-});
