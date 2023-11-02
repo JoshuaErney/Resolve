9d37005497a754feadadeb64527dc9eeadd6aec5
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2023-11-01 20:53:24 -0400

    #FactoryReset

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('themeSwitch').addEventListener('change', function(event) {
-    if (event.target.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
-  });
-  
